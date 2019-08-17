1473e3145812ddf1c46e0e5b82c8bd17b9894500
Victor Naito <victor.naito@ms.senac.br>
2019-08-17 11:32:04 -0400

    teste heroku

--- Modelo.Application/Program.cs
+++ Modelo.Application/Program.cs
@@ -19,6 +19,6 @@ namespace Modelo.Application
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseKestrel();
     }
 }
